e7f1d4a1d391a8d77511e0f6c59aae2dafbb28e5
Ross Bermudez <rossana.bermudezd@gmail.com>
2023-07-30 22:38:14 +0000

    fix image and posts

--- src/model/Education.ts
+++ src/model/Education.ts
@@ -1,9 +1,11 @@
+import type { EntryFieldTypes } from 'contentful';
+
 export interface Education {
-	institution: string;
-	degree: string;
-	startDate: Date;
-	endDate: Date;
-	description: string;
-	type: string;
+	institution: EntryFieldTypes.Text;
+	degree: EntryFieldTypes.Text;
+	startDate: EntryFieldTypes.Date;
+	endDate: EntryFieldTypes.Date;
+	description: EntryFieldTypes.RichText;
+	type: EntryFieldTypes.Text;
 	courses: string[];
 }
