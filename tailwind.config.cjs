5adb485f5ea97a49ec26896cced88acc3ffd8ae1
Ross Bermudez <rbermudez@anaconda.com>
2023-08-06 13:59:18 +0200

    Add pre-commit hooks

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,78 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 import colors from 'tailwindcss/colors';
-import {createThemes} from 'tw-colors';
+import { createThemes } from 'tw-colors';
 
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
-    safelist: [
-        'w-1/5',
-        'w-2/5',
-        'w-3/5',
-        'w-4/5',
-        'w-5/5',
-    ],
-    plugins: [
-        require('flowbite/plugin'),
-        require('tailwind-scrollbar')({nocompatible: true}),
-        function ({addBase, theme}) {
-            addBase({
-                h1: {
-                    fontSize: theme('fontSize.3xl'),
-                },
-                h2: {
-                    fontSize: theme('fontSize.2xl'),
-                },
-                h3: {
-                    fontSize: theme('fontSize.xl'),
-                },
-                p: {
-                    marginBottom: '1em'
-                }
-            });
-        },
-        createThemes({
-            light: {
-                text: colors.slate[900],
-                background: colors.sky[100],
-                base: colors.white,
-                primary: {
-                    ...colors.sky,
-                    DEFAULT: colors.sky[600],
-                },
-                secondary: {
-                    ...colors.cyan,
-                    DEFAULT: colors.cyan[600],
-                },
-                accent: {
-                    ...colors.pink,
-                    DEFAULT: colors.pink[600],
-                },
-                tertiary: {
-                    ...colors.slate,
-                    DEFAULT: colors.slate[600],
-                },
-            },
-            dark: {
-                text: 'hsl(189, 87%, 97%)',
-                background: 'hsl(0, 0%, 0%)',
-                base: colors.white,
-                primary: {
-                    ...colors.sky,
-                    DEFAULT: colors.sky[300],
-                },
-                secondary: {
-                    ...colors.cyan,
-                    DEFAULT: colors.cyan[300],
-                },
-                accent: {
-                    ...colors.pink,
-                    DEFAULT: colors.pink[300],
-                },
-                tertiary: {
-                    ...colors.slate,
-                    DEFAULT: colors.slate[300],
-                },
-            }
-        })
-    ],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
+	safelist: ['w-1/5', 'w-2/5', 'w-3/5', 'w-4/5', 'w-5/5'],
+	plugins: [
+		require('flowbite/plugin'),
+		require('tailwind-scrollbar')({ nocompatible: true }),
+		function ({ addBase, theme }) {
+			addBase({
+				h1: {
+					fontSize: theme('fontSize.3xl'),
+				},
+				h2: {
+					fontSize: theme('fontSize.2xl'),
+				},
+				h3: {
+					fontSize: theme('fontSize.xl'),
+				},
+				p: {
+					marginBottom: '1em',
+				},
+			});
+		},
+		createThemes({
+			light: {
+				text: colors.slate[900],
+				background: colors.sky[100],
+				base: colors.white,
+				primary: {
+					...colors.sky,
+					DEFAULT: colors.sky[600],
+				},
+				secondary: {
+					...colors.cyan,
+					DEFAULT: colors.cyan[600],
+				},
+				accent: {
+					...colors.pink,
+					DEFAULT: colors.pink[600],
+				},
+				tertiary: {
+					...colors.slate,
+					DEFAULT: colors.slate[600],
+				},
+			},
+			dark: {
+				text: 'hsl(189, 87%, 97%)',
+				background: 'hsl(0, 0%, 0%)',
+				base: colors.white,
+				primary: {
+					...colors.sky,
+					DEFAULT: colors.sky[300],
+				},
+				secondary: {
+					...colors.cyan,
+					DEFAULT: colors.cyan[300],
+				},
+				accent: {
+					...colors.pink,
+					DEFAULT: colors.pink[300],
+				},
+				tertiary: {
+					...colors.slate,
+					DEFAULT: colors.slate[300],
+				},
+			},
+		}),
+	],
 };
