c2331b6454fecefc41d7d55b5a3de6ef22d9b850
Ross Bermudez <rbermudez@anaconda.com>
2023-08-17 15:04:51 +0200

    fix CV page
    Signed-off-by: Ross Bermudez <rbermudez@anaconda.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import alpinejs from '@astrojs/alpinejs';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
@@ -7,7 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), alpinejs(), mdx()],
+	integrations: [tailwind(), alpinejs()],
 	output: 'server',
 	adapter: vercel({
 		analytics: true,
