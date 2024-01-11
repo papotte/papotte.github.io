d4ce2220076448db6689288513a6d4ea3a7a2067
Ross Bermudez <rbermudez@anaconda.com>
2024-01-12 00:36:52 +0200

    improve a11y

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,17 +13,15 @@ module.exports = {
 	overrides: [
 		{
 			files: ['*.astro'],
-			// Allows Astro components to be parsed.
-			parser: 'astro-eslint-parser',
-			// Parse the script in `.astro` as TypeScript by adding the following configuration.
-			// It's the setting you need when using TypeScript.
-			parserOptions: {
-				parser: '@typescript-eslint/parser',
-				extraFileExtensions: ['.astro'],
-			},
+			plugins: ['jsx-a11y'],
+			extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-strict'],
 			rules: {
-				// override/add rules settings here, such as:
-				// "astro/no-set-html-directive": "error"
+				'astro/no-deprecated-astro-resolve': 'error',
+				'astro/no-conflict-set-directives': 'error',
+				'astro/no-unused-define-vars-in-style': 'error',
+				'astro/no-set-html-directive': 'off',
+				'astro/no-unused-css-selector': 'error',
+				'astro/semi': ['error'],
 			},
 		},
 		{
